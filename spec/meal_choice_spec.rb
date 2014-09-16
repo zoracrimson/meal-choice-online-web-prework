fc442f5c83132f22d5a1e161203382796adb7836
Katie Hoffman <ktahoffman@gmail.com>
2014-09-16 16:12:37 -0400

    add color

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -1,6 +1,3 @@
-require_relative './spec_helper'
-require_relative '../meal_choice.rb' # Code your solution in this file
-
 describe '#meal_choice' do
   it 'should default to meat' do
     meal_choice.should == "meat"
